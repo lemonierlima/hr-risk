e4da7da306bc0e1856d16f3994dfd1587746e74a
Lemonier Lima <lemonier.lima@gmail.com>
2021-10-22 00:20:54 -0300

    Correção Bug1

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,10 @@
-import {useState} from 'react';
 import Link from 'next/link';
 
 function Home(){
     return (
         <div>
             <h2>Home</h2>
-            <Contador />
+
             <Link href="/about">
                 <a>About Us</a>
             </Link>
@@ -13,20 +12,4 @@ function Home(){
     )
 }
 
-function Contador(){
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador(){
-        setContador(contador + 1);
-    }
-
-    return (
-                
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
-}
-
-export default Home
+export default Home;
